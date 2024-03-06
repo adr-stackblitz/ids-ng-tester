08a7c46b5eb930345f56a56f4420e7ecac73a62a
Aldrin Ramos <aldrin.ramos@infor.com>
2024-03-06 18:58:17 +0800

    installed ids

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AppCommonModule } from '@modules/app-common/app-common.module';
+import { DataGridComponent } from './components/data-grid/data-grid.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AppCommonModule, DataGridComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
